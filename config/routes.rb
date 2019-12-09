56669213c593b31ca780098a636741854dab34c4
khurramdbg <khurram.imtiaz@gmail.com>
2019-12-09 16:41:09 +0500

    App ready for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'gray', to: 'grayscale#index'
+  root 'grayscale#index'
+  get 'customer', to: 'customer#index', as: 'customer_home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
