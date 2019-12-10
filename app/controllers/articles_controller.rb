3731bc1ae2d470081cf3750a78db1ac8ef334e2f
khurramdbg <khurram.imtiaz@gmail.com>
2019-12-11 00:08:04 +0500

    articles_listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,17 @@
 class ArticlesController < ApplicationController
 
-    def def new
+    def index
+        @articles = Article.all
+    end
+
+    def new
         @article = Article.new
-        @article.errors = []
     end
     
     def create
         #render plain: params[:article].inspect
 
-        @article = Article.new(params[:article])
+        @article = Article.new(article_params)
         if @article.save
           flash[:success] = "Object successfully created"
           redirect_to article_path(@article)
@@ -18,13 +21,32 @@ class ArticlesController < ApplicationController
         end
     end
     
+def edit
+    
+    @article = Article.find(params[:id])
+end
+
+
+ def update
+        #render plain: params[:article].inspect
+
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+          flash[:success] = "Article successfully updated"
+          redirect_to article_path(@article)
+        else
+          flash[:error] = "Something went wrong with editing"
+          render 'edit'
+        end
+    end
+
 def show
     @article = Article.find(params[:id])
 end
 
     private
     def article_params
-        params.require(:article).permit(:title, :description)
+        params.require(:article).permit(:title, :description, :author)
     end
   
     
