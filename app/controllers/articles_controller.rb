bd24d43e92eea611776f0c9d93a6b63c4afc9274
khurramdbg <khurram.imtiaz@gmail.com>
2019-12-12 21:53:03 +0500

    ui changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,64 +1,62 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:edit, :update, :show, :destroy]
-    def index
-        @articles = Article.all
-    end
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-    def new
-        @article = Article.new
-    end
-    
-    def create
-        #render plain: params[:article].inspect
+  def index
+    @articles = Article.all
+  end
 
-        @article = Article.new(article_params)
-        if @article.save
-          flash[:success] = "Object successfully created"
-          redirect_to article_path(@article)
-        else
-          flash[:error] = "Something went wrong"
-          render 'new'
-        end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #render plain: params[:article].inspect
+
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Object successfully created"
+      redirect_to article_path(@article)
+    else
+      flash[:error] = "Something went wrong"
+      render "new"
     end
-    
-def edit    
+  end
+
+  def edit
     #@article = Article.find(params[:id])
-end
+  end
 
+  def update
+    #render plain: params[:article].inspect
 
- def update
-        #render plain: params[:article].inspect
-
-        #@article = Article.find(params[:id])
-        if @article.update(article_params)
-          flash[:success] = "Article successfully updated"
-          redirect_to article_path(@article)
-        else
-          flash[:error] = "Something went wrong with editing"
-          render 'edit'
-        end
+    #@article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article successfully updated"
+      redirect_to article_path(@article)
+    else
+      flash[:error] = "Something went wrong with editing"
+      render "edit"
     end
+  end
 
-def destroy
+  def destroy
     #@article = Article.find(params[:id])
     @article.destroy
     flash[:success] = "Article removed successfully"
     redirect_to articles_path
-end
+  end
 
-def show
+  def show
     #?@article = Article.find(params[:id])
-end
+  end
 
-    private
+  private
 
-    def set_article
-        @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    def article_params
-        params.require(:article).permit(:title, :description, :author)
-    end
-  
-    
+  def article_params
+    params.require(:article).permit(:title, :description, :author)
+  end
 end
